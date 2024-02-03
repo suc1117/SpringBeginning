dc7d964ab8a189bf9245590c169f89ba45c5b0eb
suc1117 <suc1117@naver.com>
2024-02-03 21:43:15 +0900

    * Backend development - Example(Member Management) - save, find * Backend development - Test Cases

---
+++ src/main/java/cs/spring/repository/MemoryMemberRepository.java
@@ -0,0 +1,40 @@
+package cs.spring.repository;
+
+import cs.spring.domain.Member;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static AtomicLong sequence = new AtomicLong(0);
+
+    @Override
+    public Member save(Member member) {
+        member.setId(sequence.addAndGet(1));      //set member id
+        store.put(member.getId(), member);              //insert member in store
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));  // returns a member that matches the given id
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))    //returns a member that matches the given name
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values()); //return all members
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
---
+++ src/test/java/cs/spring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,64 @@
+package cs.spring.repository;
+
+import cs.spring.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class MemoryMemberRepositoryTest {
+
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    // Clear the shared data at the end of each test case
+    @AfterEach
+    public void afterEach() {
+        repository.clearStore();
+    }
+
+    @Test
+    public void save() {
+        Member member = new Member();
+        member.setName("Test");
+        repository.save(member);
+
+        Member result = repository.findById(member.getId()).get();  //Optional
+        assertThat(result).isEqualTo(member);    //Check that both the result(actual) and the member(expected) are same
+    }
+
+    @Test
+    public void findByName() {
+        Member member1 = new Member();
+        member1.setName("Test1");
+        repository.save(member1);
+
+        //Shift + F6 = rename all
+        Member member2 = new Member();
+        member2.setName("Test2");
+        repository.save(member2);
+
+        Member result = repository.findByName("Test1").get();
+        //Member result = repository.findByName("Test2").get(); //Fail
+
+        assertThat(result).isEqualTo(member1);
+    }
+
+
+    @Test
+    public void findAll() {
+        Member member1 = new Member();
+        member1.setName("Test1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("Test2");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+
+        assertThat(result.size()).isEqualTo(2);
+    }
+}
