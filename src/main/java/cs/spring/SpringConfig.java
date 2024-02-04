cefbfe3468925ae1d76a48f23bb1c8b141d2ab2f
suc1117 <suc1117@naver.com>
2024-02-04 18:55:37 +0900

    * Backend development - Example(Member Management) - Spring Data JPA (with H2 DB)

--- src/main/java/cs/spring/SpringConfig.java
+++ src/main/java/cs/spring/SpringConfig.java
@@ -14,22 +14,24 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SpringConfig {
 
-    private EntityManager em;
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
+    /*
     @Bean
     public MemberRepository memberRepository() {
         //return new MemoryMemberRepository();
-        return new JpaMemberRepository(em);   //구현 클래스를 변경하는 경우 단순히 반환값만 바꿔주면 된다. (OCP, Open-Closed Principle)
-                                            //확장은 열려있고 변경, 수정은 닫혀있다.
+        return new JpaMemberRepository(em);     //구현 클래스를 변경하는 경우 단순히 반환값만 바꿔주면 된다. (OCP, Open-Closed Principle)
+                                                //확장은 열려있고 변경, 수정은 닫혀있다.
     }
+     */
 }
--- src/main/java/cs/spring/repository/SpringDataJpaMemberRepository.java
+++ src/main/java/cs/spring/repository/SpringDataJpaMemberRepository.java
@@ -1,2 +1,14 @@
-package cs.spring.repository;public class SpringDataJpaMemberRepository {
+package cs.spring.repository;
+
+import cs.spring.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+//Spring Data JPA 가 JpaRepository를 가지고 있으면 인터페이스에 대한 구현체를 만들어서 등록해준다. (구현 클래스 필요 없음)
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+
+    //JPQL select m from Member m where m.name = ? <- findBy__ 필요한 부분을 넣으면 알아서 쿼리를 만들어서 구현함.
+    @Override
+    Optional<Member> findByName(String name);
 }
