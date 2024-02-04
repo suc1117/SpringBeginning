666cbd948e7190924636ce36c6419a8a12a775f8
suc1117 <suc1117@naver.com>
2024-02-04 19:23:28 +0900

    CI 작업 시, 테스트 케이스에 쓰이는 H2 DB를 In-memory 방식으로 변경

--- src/test/java/cs/spring/service/MemberServiceIntegrationTest.java
+++ src/test/java/cs/spring/service/MemberServiceIntegrationTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -16,6 +17,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @Transactional
 class MemberServiceIntegrationTest {
 
